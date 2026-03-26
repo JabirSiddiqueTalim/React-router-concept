600f8456b857a6ceb66430d600256decdf07f533
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-03-27 05:22:39 +0600

    Understand The Concept of Routing

---
+++ src/component/Laptop/Laptop.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Laptop = () => {
+  return (
+    <div>
+      <h2>This is Laptop</h2>
+      
+    </div>
+  );
+};
+
+export default Laptop;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,26 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Header from './component/Header/Header.jsx';
+import Root from './component/Root/Root.jsx';
+
+import Home from './component/Home/Home.jsx';
+import Moblies from './component/Mobiles/Mobiles.jsx';
+import Mobiles from './component/Mobiles/Mobiles.jsx';
+import Laptop from './component/Laptop/Laptop.jsx';
+
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    children :[
+      {index:true,Component: Home},
+      {path:'mobiles',Component: Mobiles},
+      {path :'laptop',Component: Laptop}
+    ]
   },
+  
 ]);
 
 createRoot(document.getElementById('root')).render(
